cf248143964d5df5a4e8929e866b9c59425bef32
sanayasanghvi <sanayasanghvi80@gmail.com>
2015-12-07 01:04:42 -0500

    Initial Commit

---
+++ Tip Calculator SwiftTests/Tip_Calculator_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  Tip_Calculator_SwiftTests.swift
+//  Tip Calculator SwiftTests
+//
+//  Created by macbookair11 on 12/7/15.
+//  Copyright © 2015 Sanaya Sanghvi. All rights reserved.
+//
+
+import XCTest
+@testable import Tip_Calculator_Swift
+
+class Tip_Calculator_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
